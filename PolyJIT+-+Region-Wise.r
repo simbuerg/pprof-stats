a02f2ca3a0e3552e00ad9fb9d7cd77818679df53
Andreas Simbuerger <simbuerg@fim.uni-passau.de>
2016-11-10 00:05:15 +0100

    Temp Mods for PLDI

--- PolyJIT+-+Region-Wise.r
+++ PolyJIT+-+Region-Wise.r
@@ -75,10 +75,20 @@ bar_plot <- function(plot_data) {
 #    '0f42573f-973d-4cd4-bfac-b71961d100ed',
 #    '09de35a6-f0af-4577-91a2-a564b63ef900'
 #))
+
+#data <- rbind(data, plot_comparison_data(c(
+#    '917f3844-063a-45ef-8397-c55818c37dd4',
+#    'a1116f0d-81c5-456e-a9de-6232c51df483',
+#    'cab8443e-1172-4246-8bed-59ac62a133ec'
+#)))
+
+
 polybench <- plot_comparison_data(c('9fd28f1b-8a9e-4d99-b495-e3b18fe9d347'))
 llvm_nt <-plot_comparison_data(c('45d076f4-52b7-4d38-8cea-4e1d64714aed'))
 benchbuild <- plot_comparison_data(c('d5c856a3-4f78-4988-b43f-a349d246eab2'))
 
+#data <- rbind(data, polybench)
+#data <- rbind(data, llvm_nt)
 data <- rbind(polybench, llvm_nt)
 data <- rbind(data, benchbuild)
 
@@ -86,12 +96,13 @@ data$region_name <- as.numeric(factor(data$region_name))
 data <- data[data$runtime_jit > 1000,]
 data <- transform(data, speedup = ifelse(speedup >= 1, speedup, -1/speedup))
 data <- data[complete.cases(data),]
-data
+data <- data[!is.infinite(data$speedup),]
+#data
 
 box_plot <- function(plot_data) {
-  plot <- ggplot(data = plot_data, aes(y=speedup, x=cores, color=cores)) +
+  plot <- ggplot(data = plot_data, aes(y=speedup, x=cores)) +
     geom_violin(trim= TRUE, adjust = 0.5) +
-    geom_jitter(height=0, size=0.5) +
+    #geom_jitter(height=0, size=0.5) +
     geom_hline(yintercept=0) +
     coord_cartesian(ylim = c(min(plot_data$speedup),max(plot_data$speedup))) +
     xlab("Number of cores") +
@@ -101,20 +112,23 @@ box_plot <- function(plot_data) {
   return(plot)
 }
 
-pos <- data[(as.numeric(data$cores) %in% c(1,2,5)),]
+pos <- data[(as.numeric(data$cores) %in% c(1,2,3,4,5)),]
+pos <- pos[pos$speedup < 50,]
 pos <- pos[pos$speedup > 1.1,]
-neg <- data[(as.numeric(data$cores) %in% c(1,2,5)),]
+neg <- data[(as.numeric(data$cores) %in% c(1,2,3,4,5)),]
 neg <- neg[neg$speedup < -1.1,]
+neg <- neg[neg$speedup > -50,]
 #data_pos <- data_filter[(data_filter$speedup > 0.50),]
 
 neg$t <- "bad"
 pos$t <- "good"
 
+all <- rbind(neg, pos)
+
 box_plot(all)
 box_plot(pos)
 box_plot(neg)
 
-all <- rbind(neg, pos)
 pdf(file = "./box-plot_all.pdf")
 box_plot(all)
 dev.off()
@@ -126,3 +140,12 @@ dev.off()
 pdf(file = "./box-plot_neg.pdf")
 box_plot(neg)
 dev.off()
+
+
+projects <- levels(as.factor(data$project))
+for (p in projects) {
+  p.data <- data[data$project == p,]
+  cat(p)
+  print(p.data)
+}
+
